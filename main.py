96c043cc1fdc2a9922ec9ecbbbc5c94789217bef
Mr.Helper <86283864+CGToxicBoy@users.noreply.github.com>
2021-12-01 01:26:52 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import random
+import string
+
+lower = "a b c d e f g h i j k l m n o p q r s t u v w x y z"
+
+upper = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z"
+
+number = "0 1 2 3 4 5 6 7 8 9"
+
+all = lower + upper + number
+
+password = ''.join([random.choice(string.ascii_letters + string.digits) for i in range(8)])
+
+print(password)
